be4aa092ce5ded5406ee473bfe38afbe5b738e30
Kjoonyoung <jy981110@gmail.com>
2023-06-29 17:56:24 +0900

    update

--- app/src/main/java/proj/InfoPanel.java
+++ app/src/main/java/proj/InfoPanel.java
@@ -3,7 +3,6 @@ package proj;
 import java.awt.Color;
 import java.util.Vector;
 
-import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -19,7 +18,6 @@ public class InfoPanel extends JPanel{
     Vector<String> dNames; //콤보박스를 눌러 부서명을 고를수있게
     Vector<Vector<String>> rowData; //해당 데이터의 내용을 출력 (ex: 이름 전화번호, 이메일)
     InfoPanel(){
-
         DBHandler dh = new DBHandler();
         dh.executeSelect();
         dNames = dh.getColumnData();
@@ -27,15 +25,13 @@ public class InfoPanel extends JPanel{
 
         dNameBox = new JComboBox<String>(dNames);
         dNameBox.setBackground(new Color(Prop.COLOR_MAIN));
-        dNameBox.setForeground(Color.WHITE);
+        //dNameBox.setForeground(Color.GRAY);
         infoTable = new JTable(rowData,dNames);
         infoScroll = new JScrollPane(infoTable);
-        JButton j = new JButton("click"); //가짜
 
-        infoJ = new JPanel();
-        infoJ.setBorder(new EmptyBorder(15, 5, 15, 5));
-        infoJ.add(infoScroll);
-        infoJ.add(j);
+        setBorder(new EmptyBorder(15, 5, 15, 5));
+        add(dNameBox);
+        add(infoScroll);
     }
 }
 //executeSelect, getColumnData, getData 
