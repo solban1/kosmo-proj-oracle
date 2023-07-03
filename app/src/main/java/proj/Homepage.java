57c2ac676e224b2fd2a1821f510230e7533fe277
hyundong0203 <hyunndong@nate.com>
2023-07-03 18:45:13 +0900

    UPDATE

--- app/src/main/java/proj/Homepage.java
+++ app/src/main/java/proj/Homepage.java
@@ -9,6 +9,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -28,29 +30,29 @@ public class Homepage extends JPanel {
     JButton bS1 = new JButton("등록하기");
     JButton bS2 = new JButton("수정하기");
     JButton bS3 = new JButton("지우기");
+
+    JButton CalendarButton = new JButton("일정");
     JButton downb4 = new JButton();
     int exitnum = 0;
- 
+
     JButton logButton = new JButton("출근 버튼을 눌러주세요");
 
-
     Homepage() {
+
         Date date = new Date();
-        System.out.println(date);
-        SimpleDateFormat simpl = new SimpleDateFormat("yy.MM.dd");//현재날짜 버튼 객체
+        //System.out.println(date);
+        SimpleDateFormat simpl = new SimpleDateFormat("yy.MM.dd");// 현재날짜 버튼 객체
         String s = simpl.format(date);
-        
 
         JPanel pp3 = new JPanel();
         setLayout(new BorderLayout());
         pp3.setLayout(new GridLayout(2, 3));
 
-        JButton downb1 = new JButton("일정");
-        JButton downb2 = new JButton(s); //현재 날짜
+        JButton downb2 = new JButton(s); // 현재 날짜
         JButton downb3 = new JButton("업무");
         JButton downb4 = new JButton("로그아웃");
 
-        pp3.add(downb1);
+        pp3.add(CalendarButton);
         pp3.add(downb2);
         pp3.add(downb3);
         pp3.add(downb4);
@@ -78,14 +80,12 @@ public class Homepage extends JPanel {
 
         btnpanel.setLayout(new GridLayout(1, 3));
         JButton workButton = new JButton("출근");
-     
+
         btnpanel.add(workButton);
         btnpanel.add(logButton);
-     
 
         workButton.addActionListener(new workbtnActionListener());
-
-
+        CalendarButton.addActionListener(new HandlerCalButton(this));
 
         JPanel sP = new JPanel();
         sP.setLayout(new GridLayout(1, 3)); // 수정 지우기 삭제 버튼
@@ -96,10 +96,7 @@ public class Homepage extends JPanel {
         pp2.add(pp3, BorderLayout.NORTH);
         pp2.add(pp1, BorderLayout.SOUTH);
 
-
-
         pp1.add(btnpanel, BorderLayout.NORTH); // 수정
-      
 
         add(pp1, BorderLayout.CENTER);
         add(pp2, BorderLayout.NORTH);
@@ -108,42 +105,34 @@ public class Homepage extends JPanel {
 
         workButton.setBorderPainted(false);
         logButton.setBorderPainted(false);
-      
-        downb1.setBorderPainted(false);
+
+        CalendarButton.setBorderPainted(false);
         downb2.setBorderPainted(false);
-        downb3.setBorderPainted(false);
+        CalendarButton.setBorderPainted(false);
         downb4.setBorderPainted(false);
 
         workButton.setFont(new Font("맑은고딕", Font.BOLD, 20));
         logButton.setFont(new Font("맑은고딕", Font.BOLD, 13));
-      
 
-        downb1.setFont(new Font("궁서체", Font.BOLD, 13));
+        CalendarButton.setFont(new Font("궁서체", Font.BOLD, 13));
         downb2.setFont(new Font("맑은고딕", Font.BOLD, 13));
-        downb3.setFont(new Font("맑은고딕", Font.BOLD, 13));
+        CalendarButton.setFont(new Font("맑은고딕", Font.BOLD, 13));
         downb4.setFont(new Font("맑은고딕", Font.BOLD, 13));
 
-
-
-        
- 
-
         downb4.addActionListener(new HhandlerButton(this)); // 로그아웃 버튼
-   
-
-
 
         workButton.setBackground(new Color(Prop.COLOR_MAIN));
         logButton.setBackground(new Color(Prop.COLOR_MAIN));
-      
-  
-        upb.setBackground(new Color(002, 170, 178));
-        downb1.setBackground(new Color(180, 239, 236));
-        downb2.setBackground(new Color(188, 206, 178));
-        downb3.setBackground(new Color(207, 255, 229));
-        downb4.setBackground(new Color(170, 240, 209));
 
+        upb.setBackground(new Color(002, 170, 178));
+        downb3.setBackground(new Color(180, 239, 236));
+        downb2.setBackground(new Color(188, 206, 178));
+        CalendarButton.setBackground(new Color(207, 255, 229));
+        downb4.setBackground(new Color(170, 240, 209));
+       
     }
+
+    
 
     class workbtnActionListener implements ActionListener {
 
@@ -152,9 +141,7 @@ public class Homepage extends JPanel {
             JButton wbtn = (JButton) e.getSource();
             wbtn.setBorderPainted(false);
 
-            
             Date date1 = new Date();
-            
             SimpleDateFormat simpl = new SimpleDateFormat("yy.MM.dd hh:mm aaa");
             String s1 = simpl.format(date1);
             System.out.println(date1);
@@ -164,7 +151,6 @@ public class Homepage extends JPanel {
 
             else
                 wbtn.setText("출근");
-
 
             if (wbtn.getText() == "출근") {
 
@@ -192,38 +178,48 @@ public class Homepage extends JPanel {
         }
     }
 
+    class HhandlerButton implements ActionListener {
+        Homepage hp;
+
+        HhandlerButton(Homepage hp) {
+            this.hp = hp;
+
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            UIManager.put("OptionPane.background", new Color(Prop.COLOR_MAIN)); // UIOPTION 색상
+            UIManager.put("Panel.background", new Color(Prop.COLOR_MAIN));
+            UIManager.put("Button.background", new Color(194, 177, 227));
+
+            System.out.println("로그아웃 버튼 클릭");
+            int i = JOptionPane.showConfirmDialog(null, "로그아웃하시겠습니까?", "로그아웃", JOptionPane.YES_NO_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE);
+
+            if (i == JOptionPane.OK_OPTION) {
+                LogUI lu = new LogUI();
+                lu.init();
+                lu.setUI();
+                SwingUtilities.getWindowAncestor(hp).setVisible(false);
+            }
+        }
+    }
+    class HandlerCalButton implements ActionListener{
+        Homepage hp;
+
+        HandlerCalButton(Homepage hp) {
+                this.hp = hp;
+
+        }
+        @Override  // 일정버튼 누르면 일정표로 가게만드는 
+        public void actionPerformed(ActionEvent e) {
+        ((MainUI)SwingUtilities.getAncestorOfClass(MainUI.class, hp)).change(new Calendar());   
+            
+        }
+    }
     public static void main(String[] args) {
         Homepage h = new Homepage();
 
     }
 
-}
-
-
-class HhandlerButton implements ActionListener {
-    Homepage hp;
-
-    HhandlerButton(Homepage hp) {
-        this.hp = hp;
-
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        UIManager.put("OptionPane.background", new Color(Prop.COLOR_MAIN)); // UIOPTION 색상
-        UIManager.put("Panel.background", new Color(Prop.COLOR_MAIN));
-        UIManager.put("Button.background", new Color(194, 177, 227));
-
-        System.out.println("로그아웃 버튼 클릭");
-        int i = JOptionPane.showConfirmDialog(null, "로그아웃하시겠습니까?", "로그아웃", JOptionPane.YES_NO_OPTION,
-                JOptionPane.INFORMATION_MESSAGE);
-
-        if (i == JOptionPane.OK_OPTION) {
-            LogUI lu = new LogUI();
-            lu.init();
-            lu.setUI();
-            SwingUtilities.getWindowAncestor(hp).setVisible(false);
-
-        }    
-    }
 }
