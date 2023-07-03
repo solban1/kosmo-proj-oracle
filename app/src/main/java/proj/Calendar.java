dd2cbf887c194d16c4433d518d38ecdcddc229d6
Kjoonyoung <jy981110@gmail.com>
2023-07-03 11:35:20 +0900

    update

--- app/src/main/java/proj/Calendar.java
+++ app/src/main/java/proj/Calendar.java
@@ -1,34 +1,44 @@
 package proj;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 
 public class Calendar extends JFrame {
-
-
+    String weekList[] = {"일","월","화","수","목","금","토"};
+    JButton lastweekButton, nextweekButton;
+    JTable calendarTable;
+    JPanel calPanel;
+    Container cp;
     Calendar(){
-   
-	
-   
-        setUi();  
-    
+        setLayout(new BorderLayout());
 
-         
-    }  
-    
-    void setUi(){
-        setTitle("Main Home");
-        setSize(300, 500);
+        lastweekButton = new JButton("<-");
+        nextweekButton = new JButton("->");
+        for(int i=0; i<weekList.length;i++){
+            
+        }
+        calPanel.add(lastweekButton);
+        calPanel.add(nextweekButton);
+        
+        ((JFrame) cp).getContentPane();
+        cp.add(calPanel);
+        setUI();
+    }
+    void setUI(){
+        setTitle("LogIn");
+        setSize(Prop.WIN_WIDTH, Prop.WIN_HEIGHT);
         setVisible(true);
         setLocationRelativeTo(null);
 
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-
+    public static void main(String[] args){
+        new Calendar();
+    }
 }
