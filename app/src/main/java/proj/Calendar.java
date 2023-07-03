c1cdc6765af660ab0bb5c9ca620cbe23a968034f
Kjoonyoung <jy981110@gmail.com>
2023-07-03 11:43:15 +0900

    update

--- app/src/main/java/proj/Calendar.java
+++ app/src/main/java/proj/Calendar.java
@@ -1,19 +1,15 @@
 package proj;
 
 import java.awt.BorderLayout;
-import java.awt.Container;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 
-public class Calendar extends JFrame {
+public class Calendar extends JPanel {
     String weekList[] = {"일","월","화","수","목","금","토"};
     JButton lastweekButton, nextweekButton;
     JTable calendarTable;
-    JPanel calPanel;
-    Container cp;
     Calendar(){
         setLayout(new BorderLayout());
 
@@ -22,23 +18,11 @@ public class Calendar extends JFrame {
         for(int i=0; i<weekList.length;i++){
             
         }
-        calPanel.add(lastweekButton);
-        calPanel.add(nextweekButton);
-        
-        ((JFrame) cp).getContentPane();
-        cp.add(calPanel);
-        setUI();
-    }
-    void setUI(){
-        setTitle("LogIn");
-        setSize(Prop.WIN_WIDTH, Prop.WIN_HEIGHT);
-        setVisible(true);
-        setLocationRelativeTo(null);
+        calendarTable(weekList,null);
+        add(lastweekButton);
+        add(nextweekButton);
 
-        setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    public static void main(String[] args){
-        new Calendar();
+    private void calendarTable(String[] weekList2, Object object) {
     }
 }
