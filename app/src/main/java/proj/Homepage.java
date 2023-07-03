50b6bab4d011fefba542851a7198394c3f9038aa
hyundong0203 <hyunndong@nate.com>
2023-07-03 16:23:39 +0900

    update

--- app/src/main/java/proj/Homepage.java
+++ app/src/main/java/proj/Homepage.java
@@ -11,6 +11,9 @@ import java.awt.event.ActionListener;
 import java.io.BufferedInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.swing.ImageIcon;
 
@@ -29,24 +32,31 @@ public class Homepage extends JPanel {
     JLabel jL1, jL2;
     JOptionPane jp;
     Container con;
-    JButton[] btn = new JButton[5];
     JButton bN1 = new JButton("출근");
     JButton bN2 = new JButton("퇴근");
     JButton bN3 = new JButton("정보");
     JButton bS1 = new JButton("등록하기");
     JButton bS2 = new JButton("수정하기");
     JButton bS3 = new JButton("지우기");
-    JButton downb4 = new JButton("로그아웃");
+    JButton downb4 = new JButton();
     int exitnum = 0;
+ 
+    JButton logButton = new JButton("출근 버튼을 눌러주세요");
+
 
     Homepage() {
+        Date date = new Date();
+        System.out.println(date);
+        SimpleDateFormat simpl = new SimpleDateFormat("yy.MM.dd");//현재날짜 버튼 객체
+        String s = simpl.format(date);
+        
 
         JPanel pp3 = new JPanel();
         setLayout(new BorderLayout());
         pp3.setLayout(new GridLayout(2, 3));
 
         JButton downb1 = new JButton("일정");
-        JButton downb2 = new JButton("현재 날짜");
+        JButton downb2 = new JButton(s); //현재 날짜
         JButton downb3 = new JButton("업무");
         JButton downb4 = new JButton("로그아웃");
 
@@ -78,22 +88,14 @@ public class Homepage extends JPanel {
 
         btnpanel.setLayout(new GridLayout(1, 3));
         JButton workButton = new JButton("출근");
-        JButton logButton = new JButton("로그1");
-        JButton logButton1 = new JButton("로그2");
-
+     
         btnpanel.add(workButton);
         btnpanel.add(logButton);
-        btnpanel.add(logButton1);
+     
 
         workButton.addActionListener(new workbtnActionListener());
 
-        // JPanel Np = new JPanel(); // 버튼 출퇴근 버튼
 
-        // setLayout(new BorderLayout());
-
-        // Np.setLayout(new GridLayout(1, 2));
-        // Np.add(bN1);
-        // Np.add(bN2);
 
         JPanel sP = new JPanel();
         sP.setLayout(new GridLayout(1, 3)); // 수정 지우기 삭제 버튼
@@ -104,48 +106,47 @@ public class Homepage extends JPanel {
         pp2.add(pp3, BorderLayout.NORTH);
         pp2.add(pp1, BorderLayout.SOUTH);
 
-        // pp1.add(Np, BorderLayout.NORTH);
+
 
         pp1.add(btnpanel, BorderLayout.NORTH); // 수정
-        pp1.add(sP, BorderLayout.SOUTH);
+      
 
         add(pp1, BorderLayout.CENTER);
         add(pp2, BorderLayout.NORTH);
 
         upb.setBorderPainted(false); // 버튼 윤곽선 지우기
-        
+
         workButton.setBorderPainted(false);
         logButton.setBorderPainted(false);
-        logButton1.setBorderPainted(false);
+      
         downb1.setBorderPainted(false);
         downb2.setBorderPainted(false);
         downb3.setBorderPainted(false);
         downb4.setBorderPainted(false);
 
-        bN1.setFont(new Font("맑은고딕", Font.BOLD, 20)); // 글꼴
-        bN2.setFont(new Font("맑은고딕", Font.BOLD, 20));
-        bS1.setFont(new Font("맑은고딕", Font.BOLD, 12));
-        bS2.setFont(new Font("맑은고딕", Font.BOLD, 12));
-        bS3.setFont(new Font("맑은고딕", Font.BOLD, 12));
+        workButton.setFont(new Font("맑은고딕", Font.BOLD, 20));
+        logButton.setFont(new Font("맑은고딕", Font.BOLD, 13));
+      
 
         downb1.setFont(new Font("궁서체", Font.BOLD, 13));
         downb2.setFont(new Font("맑은고딕", Font.BOLD, 13));
         downb3.setFont(new Font("맑은고딕", Font.BOLD, 13));
         downb4.setFont(new Font("맑은고딕", Font.BOLD, 13));
 
-        bN1.addActionListener(new Hhandler(this)); // 출근
-        bN2.addActionListener(new Hhandler(this)); // 퇴근
-        bS1.addActionListener(new Hhandler(this)); // 수정
-        bS2.addActionListener(new Hhandler(this)); // 수정하기
-        bS3.addActionListener(new Hhandler(this)); // 지우기
+
+
+        
+ 
 
         downb4.addActionListener(new HhandlerButton(this)); // 로그아웃 버튼
+   
 
-        bN1.setBackground(new Color(Prop.COLOR_MAIN));
-        bN2.setBackground(new Color(Prop.COLOR_MAIN));
-        bS1.setBackground(new Color(Prop.COLOR_MAIN));
-        bS2.setBackground(new Color(Prop.COLOR_MAIN));
-        bS3.setBackground(new Color(Prop.COLOR_MAIN));
+
+
+        workButton.setBackground(new Color(Prop.COLOR_MAIN));
+        logButton.setBackground(new Color(Prop.COLOR_MAIN));
+      
+  
         upb.setBackground(new Color(002, 170, 178));
         downb1.setBackground(new Color(180, 239, 236));
         downb2.setBackground(new Color(188, 206, 178));
@@ -153,94 +154,61 @@ public class Homepage extends JPanel {
         downb4.setBackground(new Color(170, 240, 209));
 
     }
-    class workbtnActionListener implements ActionListener{
 
-    @Override
-    public void actionPerformed(ActionEvent e){ //출근 퇴근 버튼 액션리스너
-         JButton wbtn = (JButton)e.getSource();
-         wbtn.setBorderPainted(false);
+    class workbtnActionListener implements ActionListener {
 
-         if(wbtn.getText().equals("출근"))
-            wbtn.setText("퇴근");          
+        @Override
+        public void actionPerformed(ActionEvent e) { // 출근 퇴근 버튼 액션리스너
+            JButton wbtn = (JButton) e.getSource();
+            wbtn.setBorderPainted(false);
+
+            
+            Date date1 = new Date();
+            
+            SimpleDateFormat simpl = new SimpleDateFormat("yy.MM.dd hh:mm aaa");
+            String s1 = simpl.format(date1);
+            System.out.println(date1);
+
+            if (wbtn.getText().equals("출근"))
+                wbtn.setText("퇴근");
+
             else
-            wbtn.setText("출근");
+                wbtn.setText("출근");
 
-        if (wbtn.getText() == "출근") {
 
-            System.out.println("출근버튼 클릭");
-            /*
-             * JOptionPane.showConfirmDialog(null, "출근하시겠습니까?", "출근",
-             * JOptionPane.YES_NO_OPTION,
-             * JOptionPane.INFORMATION_MESSAGE, icon1);
-             */
-            JOptionPane.showMessageDialog(null, "퇴근 완료", null, JOptionPane.PLAIN_MESSAGE);
-        
-        } else  {
-            System.out.println("퇴근버튼 클릭");
-            /*JOptionPane.showConfirmDialog(null, "퇴근하시겠습니까?", "퇴근", JOptionPane.YES_NO_OPTION,
-            JOptionPane.INFORMATION_MESSAGE, null);*/
-            JOptionPane.showMessageDialog(null, "출근완료", null, JOptionPane.PLAIN_MESSAGE);
-        
+            if (wbtn.getText() == "출근") {
+
+                System.out.println("출근버튼 클릭");
+                /*
+                 * JOptionPane.showConfirmDialog(null, "출근하시겠습니까?", "출근",
+                 * JOptionPane.YES_NO_OPTION,
+                 * JOptionPane.INFORMATION_MESSAGE, icon1);
+                 */
+                JOptionPane.showMessageDialog(null, "퇴근 완료", null, JOptionPane.PLAIN_MESSAGE);
+
+            } else {
+                System.out.println("퇴근버튼 클릭");
+                /*
+                 * JOptionPane.showConfirmDialog(null, "퇴근하시겠습니까?", "퇴근",
+                 * JOptionPane.YES_NO_OPTION,
+                 * JOptionPane.INFORMATION_MESSAGE, null);
+                 */
+                JOptionPane.showMessageDialog(null, "출근완료", null, JOptionPane.PLAIN_MESSAGE);
+
+            }
+
+            logButton.setText(s1);
+
         }
     }
-}
-
 
     public static void main(String[] args) {
         Homepage h = new Homepage();
 
     }
+
 }
 
-class Hhandler implements ActionListener {
-    Homepage h1;
-
-    Hhandler(Homepage h1) {
-        this.h1 = h1;
-
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        ImageIcon icon1 = new ImageIcon("res/out1.png"); // 출근 이미지
-        ImageIcon icon2 = new ImageIcon("res/go1.png"); // 퇴근이미지
-
-        UIManager.put("OptionPane.background", new Color(177, 224, 227)); // UIOPTION 색상
-        // UIManager.put("Panel.background", new Color(53, 20, 219));
-        UIManager.put("Button.background", new Color(194, 177, 227));
-
-        JButton b = (JButton) e.getSource();
-        JOptionPane jp = new JOptionPane();
-
-        if (b.equals(h1.bN1)) {
-
-            System.out.println("출근버튼 클릭");
-            /*
-             * JOptionPane.showConfirmDialog(null, "출근하시겠습니까?", "출근",
-             * JOptionPane.YES_NO_OPTION,
-             * JOptionPane.INFORMATION_MESSAGE, icon1);
-             */
-            JOptionPane.showMessageDialog(null, "출근이 완료되었습니다", null, JOptionPane.PLAIN_MESSAGE);
-
-        } else if (b.equals(h1.bN2)) {
-            System.out.println("퇴근버튼 클릭");
-            JOptionPane.showConfirmDialog(null, "퇴근하시겠습니까?", "퇴근", JOptionPane.YES_NO_OPTION,
-                    JOptionPane.INFORMATION_MESSAGE, icon2);
-        } else if (b.equals(h1.bS1)) {
-            System.out.println("등록버튼 클릭");
-            JOptionPane.showConfirmDialog(null, "등록하시겠습니까?", "등록중", JOptionPane.YES_NO_OPTION,
-                    JOptionPane.INFORMATION_MESSAGE, null);
-        } else if (b.equals(h1.bS2)) {
-            System.out.println("수정버튼 클릭");
-            JOptionPane.showConfirmDialog(null, "수정하시겠습니까?", "수정중", JOptionPane.YES_NO_OPTION,
-                    JOptionPane.INFORMATION_MESSAGE, null);
-        } else if (b.equals(h1.bS3)) {
-            System.out.println("삭제버튼 클릭");
-            JOptionPane.showConfirmDialog(null, "삭제하시겠습니까?", "삭제", JOptionPane.YES_NO_OPTION,
-                    JOptionPane.INFORMATION_MESSAGE, null);
-        }
-    }
-}
 
 class HhandlerButton implements ActionListener {
     Homepage hp;
@@ -251,22 +219,21 @@ class HhandlerButton implements ActionListener {
     }
 
     @Override
-    public void actionPerformed(ActionEvent arg0) {
+    public void actionPerformed(ActionEvent e) {
+        UIManager.put("OptionPane.background", new Color(Prop.COLOR_MAIN)); // UIOPTION 색상
+        UIManager.put("Panel.background", new Color(Prop.COLOR_MAIN));
+        UIManager.put("Button.background", new Color(194, 177, 227));
 
         System.out.println("로그아웃 버튼 클릭");
         int i = JOptionPane.showConfirmDialog(null, "로그아웃하시겠습니까?", "로그아웃", JOptionPane.YES_NO_OPTION,
                 JOptionPane.INFORMATION_MESSAGE);
+
         if (i == JOptionPane.OK_OPTION) {
             LogUI lu = new LogUI();
             lu.init();
             lu.setUI();
             SwingUtilities.getWindowAncestor(hp).setVisible(false);
-        }
 
+        }    
     }
 }
-
-
-
-
-
