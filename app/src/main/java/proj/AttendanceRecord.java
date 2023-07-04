869e8666ccff2314dc597aa2f5967a5479785e6c
hyundong0203 <hyunndong@nate.com>
2023-07-04 10:10:55 +0900

    update

--- app/src/main/java/proj/AttendanceRecord.java
+++ app/src/main/java/proj/AttendanceRecord.java
@@ -1,17 +1,26 @@
 package proj;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.event.AncestorListener;
 
-public class AttendanceRecord extends JPanel {
-
+public class AttendanceRecord extends JFrame {
+    
     AttendanceRecord() {
-
-        JLabel upLabel = new JLabel();
-        String titleName = ("Today");
+        JFrame jFrame = new JFrame();
         
+        JPanel uppPanel = new JPanel();
+        setLayout(new BorderLayout());
+        
+
+        uppPanel.setLayout(new GridLayout(1,0));
+        JButton titlebtn = new JButton("Today");
+        add(titlebtn);
+        uppPanel.add(titlebtn);
+        uppPanel.add(titlebtn, BorderLayout.NORTH);
 
     }
 }
