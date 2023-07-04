cd7d4fc2e9b971ae8a32d1d0664511a0ef6355ad
hyundong0203 <hyunndong@nate.com>
2023-07-04 18:47:58 +0900

    update

--- app/src/main/java/proj/AttendanceRecord.java
+++ app/src/main/java/proj/AttendanceRecord.java
@@ -1,62 +1,74 @@
 package proj;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.ScrollPane;
 import java.sql.Connection;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Vector;
 
 import javax.swing.BoxLayout;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+
+
 public class AttendanceRecord extends JPanel {
     JTable RecordTable;
     JLabel JLabel;
     Container cp;
-    private Connection con;
-    String data[][] ={
-            {"09:10","null","20h"},
-            {"09:15","null","20h"},
-            {"09:11","null","20h"}
-            };
-    String column []={"출근시간","퇴근시간","누적시간"};
-       
+
     AttendanceRecord() {
-        
+
         Date date = new Date();
         SimpleDateFormat simpl = new SimpleDateFormat("yy.MM.dd");// 현재날짜 버튼 객체
         String s = simpl.format(date);
-        
+
         JPanel jPanel = new JPanel();
-        JLabel titleL = new JLabel("    Today");
-        JLabel subtitleL = new JLabel(" <"+ s +">"); 
-        JLabel centerbody = new JLabel();
+        JLabel titleL = new JLabel("                    Today");
+        JLabel subtitleL = new JLabel("                   <" + s + ">");
+        
         jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
-        
+
         jPanel.add(titleL);
-        jPanel. add(subtitleL);
-        jPanel. add(centerbody);
-        add(jPanel);
+        jPanel.add(subtitleL);
         
-        JPanel centerPanel = new JPanel();
-        jPanel.add(centerPanel);
+        setLayout(new BorderLayout());
         
-        RecordTable = new JTable( data , column );  
-        RecordTable.setCellSelectionEnabled(true);
-           /* String Data = null;
-            int[] row = RecordTable.getSelectedRows();
-            int[][][] Columns = RecordTable.getSelectedColumns();*/
-        JScrollPane jScrollPane = new JScrollPane(RecordTable);
-        centerPanel.add(RecordTable);
-        RecordTable.setSize(30,30);
-
-
-
+        add(jPanel,BorderLayout.NORTH);
+        // 테이블 추가
+        
+        DBHandler db = new DBHandler();
+        db.executeSelectAttend("이현동");
+        Vector<String> columnData = db.getColumnData();
+        Vector<String> columnName = new Vector<String>();
+        columnName.add("출근시간");
+        columnName.add("퇴근시간");
+        columnName.add("업무시간");
+        Vector<Vector<String>> data = db.getData();
+        for (Vector<String> row : data) {
+            DateTimeFormatter f = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss");
+            row.add(String.valueOf(ChronoUnit.HOURS.between(LocalDateTime.parse(row.get(0), f), LocalDateTime.parse(row.get(1), f))));
+            row.add(row.get(1));
+        }
+        System.out.println(columnData);
+        RecordTable = new JTable(data,columnName);
+        JScrollPane sp = new JScrollPane();
+        sp.setViewportView(RecordTable);
+        add(sp);
+        
         titleL.setFont(new Font("맑은고딕", Font.BOLD, 20));
         subtitleL.setFont(new Font("맑은고딕", Font.BOLD, 20));
-    }
-}
 
+    }
+
+}
