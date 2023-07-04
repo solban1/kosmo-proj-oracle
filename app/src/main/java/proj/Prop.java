115a65b71669465e8ad347c28774d616b265e886
Sol-ban Bae <solban1@daum.net>
2023-07-04 17:13:46 +0900

    Add props

--- app/src/main/java/proj/Prop.java
+++ app/src/main/java/proj/Prop.java
@@ -4,4 +4,7 @@ public class Prop {
     public static final int WIN_WIDTH = 400;
     public static final int WIN_HEIGHT = 600;
     public static final int COLOR_MAIN = 0x4dbbd1;
+
+    public static String email = null;
+    public static String ename = null;
 }
