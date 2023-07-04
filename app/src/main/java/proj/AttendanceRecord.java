e5f4153e1bacfc5a0ea5b3ef86736bf89bf96d12
hyundong0203 <hyunndong@nate.com>
2023-07-04 09:53:59 +0900

    update

---
+++ app/src/main/java/proj/AttendanceRecord.java
@@ -0,0 +1,18 @@
+package proj;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.event.AncestorListener;
+
+public class AttendanceRecord extends JPanel {
+
+    AttendanceRecord() {
+
+        JLabel upLabel = new JLabel();
+        String titleName = ("Today");
+        
+
+    }
+}
+
